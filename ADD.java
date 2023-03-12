619d2d883e4167c5a4fed95ea42adaf865fa200a
sakhe <201906759@ump.ac.za>
2023-03-12 12:47:12 +0200

    first commit of an ADD app

---
+++ ADD.java
@@ -0,0 +1,9 @@
+public class ADD{
+	public static void main(String [] args){\
+		int x =5;
+		int y =5;
+		int sum=x+y;
+		System.out.println("Vriable x plus variable y is equals to"+ sum);
+
+	}
+}
